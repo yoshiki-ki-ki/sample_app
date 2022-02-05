a6c1d2f25f7aba696761fc660b29cb62cabab779
EC2 Default User <ec2-user@ip-172-31-26-5.ec2.internal>
2022-02-05 02:24:52 +0000

    [Finish]app-7

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -20,11 +20,17 @@ class ListsController < ApplicationController
   def edit
     @list = List.find(params[:id])
   end
-  
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to list_path(list.id)
+  end
+
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to '/lists'
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   get 'lists/new'
-  get 'lists/edit'
   post 'lists' => 'lists#create'
   get 'lists' => 'lists#index'
   get '/top' => 'homes#top'
   get 'lists/:id' => 'lists#show', as: 'list'
   get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
   patch 'lists/:id' => 'lists#update', as: 'update_list'
+  delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
